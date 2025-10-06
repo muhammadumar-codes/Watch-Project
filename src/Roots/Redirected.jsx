1c9b19f4a1b879c923b626060ae2b4d1b3b05c27
muhammadumar-codes <muhammadumar.codes@gmail.com>
2025-10-06 09:09:11 +0500

    update

--- src/Roots/Redirected.jsx
+++ src/Roots/Redirected.jsx
@@ -1,9 +1,8 @@
 
 
 import {Outlet,Navigate} from  "react-router-dom"
-
+// if the user is already login it  goes to home page else outlet the pages of registration or login 
 export default function Redirect (){
-
 const user=localStorage.getItem("currentUser")
 return user? <Navigate to={"/"} /> :<Outlet/>
 }
--- src/components/ProductCard/Product.jsx
+++ src/components/ProductCard/Product.jsx
@@ -1,37 +1,43 @@
 import "./Product.css";
 
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
 
 export default function Product({ watches }) {
+  const navigate = useNavigate();
+
+  // handle link to detail page
+
   return (
-
- 
+    <>
       <div className="card-container">
-      {watches.length > 0 ? (
-        watches.map((item) => (
-         <Link to={`productDetail/${item.id}`}>
-         
-          <div
-            className="card shadow-sm mb-4 bg-body rounded"
-            style={{ width: "22rem" }}
-            key={item.id}
-          >
-            <img src={item.image} className="watch-img" alt={item.name} />
-            <div className="card-body text-center">
-              <h5 className="card-title">{item.name}</h5>
-              <p className="card-text">{item.description}</p>
-              <h6 className="price-tag">Rs, {item.price}</h6>
-              <a href="#" className="btn-gradient">🛒 Add To Cart</a>
+        {watches.length > 0 ? (
+          watches.map((item) => (
+            <div
+              className="card shadow-sm mb-4 bg-body rounded"
+              style={{ width: "22rem" }}
+              key={item.id}
+            >
+              <img
+                src={item.image}
+                className="watch-img"
+                alt={item.name}
+                onClick={() => navigate(`productDetail/${item.id}`)}
+              />
+              <div className="card-body text-center">
+                <h5 className="card-title">{item.name}</h5>
+                <p className="card-text">{item.description}</p>
+                <h6 className="price-tag">Rs, {item.price}</h6>
+                <a href="#" className="btn-gradient">
+                  🛒 Add To Cart
+                </a>
+              </div>
             </div>
-          </div>
-         </Link>
-        ))
-      ) : (
-        <h3>No watches found for this category ⌚</h3>
-      )}
-    </div>
-    
-
-   
+          ))
+        ) : (
+          <h3>No watches found for this category ⌚</h3>
+        )}
+      </div>
+    </>
   );
 }
